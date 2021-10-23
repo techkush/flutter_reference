82ebbf66ab155a0c68660eb7fb91eae27a686b69
Namesh Kushantha <44038404+techkush@users.noreply.github.com>
2021-10-24 04:04:03 +0530

    app bar and banners done

--- lib/components/app_bars.dart
+++ lib/components/app_bars.dart
@@ -12,9 +12,33 @@ class _AppBarWidgetScreenState extends State<AppBarWidgetScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
+        leading: const Icon(Icons.menu),
+        title: const Text('Page title', style: TextStyle(),),
+        actions: const [
+          Icon(Icons.favorite),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Icon(Icons.search),
+          ),
+          Icon(Icons.more_vert),
+        ],
+        elevation: 3,
       ),
       body: Container(),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(icon: const Icon(Icons.menu), onPressed: () {}),
+            const Spacer(),
+            IconButton(icon: const Icon(Icons.search), onPressed: () {}),
+            IconButton(icon: const Icon(Icons.more_vert), onPressed: () {}),
+          ],
+        ),
+      ),
+      floatingActionButton:
+          FloatingActionButton(child: const Icon(Icons.add), onPressed: () {}),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
---
+++ lib/components/banners.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_reference/main.dart';
+
+// A banner displays a prominent message and related optional actions.
+class BannersWidgetScreen extends StatefulWidget {
+  const BannersWidgetScreen({Key? key}) : super(key: key);
+
+  @override
+  _BannersWidgetScreenState createState() => _BannersWidgetScreenState();
+}
+
+class _BannersWidgetScreenState extends State<BannersWidgetScreen> {
+
+  bool isOpenBanner = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Banners'),),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            isOpenBanner ? MaterialBanner(
+              content: const Text('A banner displays a prominent message and related optional actions.'),
+              leading: const CircleAvatar(child: Icon(Icons.delete)),
+              actions: [
+                TextButton(
+                  child: const Text('ACTION 1', style: buttonTextStyleActive,),
+                  onPressed: () { },
+                ),
+                TextButton(
+                  child: const Text('ACTION 2', style: buttonTextStyleActive,),
+                  onPressed: () { },
+                ),
+              ],
+            ) : Container(),
+            Positioned(
+              bottom: 20,
+              child: TextButton(
+                child: const Text('Open/Close Banner', style: buttonTextStyleActive,),
+                onPressed: () {
+                  setState(() {
+                    isOpenBanner = !isOpenBanner;
+                  });
+                },
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
