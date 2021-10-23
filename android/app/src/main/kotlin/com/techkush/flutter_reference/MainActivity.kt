749306f7172d0e8bdeb5346a6903f88ff1dd06f6
Namesh Kushantha <44038404+techkush@users.noreply.github.com>
2021-10-24 03:13:42 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/techkush/flutter_reference/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.techkush.flutter_reference
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/components/app_bars.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppBarWidgetScreen extends StatefulWidget {
+  const AppBarWidgetScreen({Key? key}) : super(key: key);
+
+  @override
+  _AppBarWidgetScreenState createState() => _AppBarWidgetScreenState();
+}
+
+class _AppBarWidgetScreenState extends State<AppBarWidgetScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        
+      ),
+      body: Container(),
+    );
+  }
+}
